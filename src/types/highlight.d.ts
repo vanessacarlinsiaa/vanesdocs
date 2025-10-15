3a062077111535495f1a125b1e1ad40c2099f626
Vanessa Carlinsia <vanessacarlinsiatjong@gmail.com>
2025-10-15 14:02:18 +0700

    feat: set dependencies - new feature [code block edit]

---
+++ src/types/highlight.d.ts
@@ -0,0 +1,13 @@
+declare module "lowlight/lib/core";
+
+declare module "highlight.js/lib/languages/javascript";
+declare module "highlight.js/lib/languages/typescript";
+declare module "highlight.js/lib/languages/python";
+declare module "highlight.js/lib/languages/java";
+declare module "highlight.js/lib/languages/c";
+declare module "highlight.js/lib/languages/cpp";
+declare module "highlight.js/lib/languages/go";
+declare module "highlight.js/lib/languages/json";
+declare module "highlight.js/lib/languages/css";
+declare module "highlight.js/lib/languages/xml";
+declare module "highlight.js/lib/languages/sql";
